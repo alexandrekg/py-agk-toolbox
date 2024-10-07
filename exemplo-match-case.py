1848670810c81f78c74c688758333c0924682581
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2024-10-07 08:39:07 -0300

    Finalizando exemplo match-case-statement

--- exemplo-match-case.py
+++ exemplo-match-case.py
@@ -2,12 +2,16 @@
 def use_case():
     command = "Hello"
     # syntax error
-    case "Hello"
+    # case "Hello"
 
     match command:
         # esse caso funciona porquê está dentro de um match
         case "Hello":
-            print('Hi')
+            print('Hello to you too!')
+        case "Goodbye, World!":
+            print('See you later!')
+        case other:
+            print('No match found')
 
 
 if __name__ == "__main__":
